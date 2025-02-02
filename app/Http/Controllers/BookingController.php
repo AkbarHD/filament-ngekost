00497b9d6d0df97349ace9a5095b36b828c1d17f
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2025-02-03 01:21:06 +0700

    redirect to booking success

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -86,4 +86,13 @@ class BookingController extends Controller
         $paymentUrl = \Midtrans\Snap::createTransaction($params)->redirect_url;
         return redirect($paymentUrl);
     }
+
+    public function success(Request $request)
+    {
+        $transaction = $this->transactionRepository->getTransactionByCode($request->order_id);
+        if (!$transaction) {
+            return redirect()->route('home');
+        }
+        return view('pages.booking.success', compact('transaction'));
+    }
 }
--- app/Interfaces/TransactionRepositoryInterface.php
+++ app/Interfaces/TransactionRepositoryInterface.php
@@ -5,8 +5,8 @@ namespace App\Interfaces;
 interface TransactionRepositoryInterface
 {
     public function getTransactionDataFromSession();
-
     public function saveTransactionDataToSession($data);
 
     public function saveTransaction($data);
+    public function getTransactionByCode($code);
 }
